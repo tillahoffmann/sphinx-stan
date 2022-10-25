dc4c21212906cc21a8225893972797998099ec4e
Till Hoffmann <tillahoffmann@gmail.com>
2022-10-25 13:17:34 -0400

    Build python wheel rather than `sdist`.

--- dodo.py
+++ dodo.py
@@ -4,10 +4,10 @@ manager = di.Manager.get_instance()
 
 manager(basename="docs", actions=[["sphinx-build", ".", "docs/_build"]])
 manager(basename="requirements", targets=["requirements.txt"], actions=[["pip-compile"]],
-        file_dep=["requirements.in", "setup.py"])
+        file_dep=["pyproject.toml", "requirements.in", "setup.py"])
 manager(basename="tests", actions=[["pytest"]])
 manager(basename="lint", actions=[["flake8"]])
 
 with di.defaults(basename="package"):
-    task = manager(name="sdist", actions=[["python", "setup.py", "sdist"]])
+    task = manager(name="sdist", actions=[["python", "-m", "build", "."]])
     manager(name="check", actions=["twine check dist/*"], task_dep=[task])
